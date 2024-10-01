37a7fd5d954ea138593cc2cb6284a1ce2de0ee8e
AndresSig2005 <andres.sigcha@udla.edu.ec>
2024-10-01 15:26:39 -0500

    commit 1

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    float notas [3];
+    char nombres [5][20]; 
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Ingrese el nombre %d: ", i); 
+        fflush(stdin); 
+        fgets(nombres[i],20,stdin); 
+
+        for (int j = 0; j < 3; j++)
+        {
+            printf("notas [%d]: ", j); 
+            scanf
+        }
+        
+    }
+}

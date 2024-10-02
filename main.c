61299d8c762f6f75453bf182c4ade06a27effed6
AndresSig2005 <andres.sigcha@udla.edu.ec>
2024-10-02 00:32:41 -0500

    commit 1

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main (int argc, char *argv[]) {
         for (int j = 0; j < 3; j++)
         {
             printf("notas [%d]: ", j); 
-            scanf
+            
         }
         
     }
